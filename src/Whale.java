6665b14967323a12ff991f625251c6f025481222
John-Ortega25 <league@WorthyMini.local>
2017-05-20 12:11:23 -0700

    5-20-17

--- src/Whale.java
+++ src/Whale.java
@@ -1,12 +1,42 @@
 
 public class Whale {
 	private int size;
-
-	public Whale(int size){
-		this.size= this.size;
+	private int age;
+	
+	public Whale(int size, int age){
+		this.size= size;
+		this.age= age;
 	}
-	public void getSize(int newSize ){
-		this.size= newSize;
-		System.out.println(newSize);
+
+	public int getSize() {
+		if (this.size > 10) {
+			System.out.println("Your whale should go on a diet!");
+		} else {
+			System.out.println("Your whale needes to eat more!");
+		}
+
+		return size;
+	}
+	public int getAge(){
+		if (this.age> 10){
+			System.out.println("Your whale has a long life to live!");
+		}else{
+			System.out.println("How is your whale still alive!");
+		}
+		return age;
+	}
+	public void setAge(int numAge){
+		this.age= numAge;
+	}
+	public void setSize( int numSize){
+		this.size= numSize;
+	}
+	public static void main(String[] args) {
+		Whale Willy= new Whale(20, 5);
+		Willy.setSize(8);
+		Willy.getSize();
+		Willy.setAge(20);
+		Willy.getAge();
+		
 	}
 }
