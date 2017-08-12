da09fba030d57d32c4378155b6da43d240d9bc31
John-Ortega25 <league@WorthyMini.local>
2017-08-12 12:22:51 -0700

    8-12-17

---
+++ src/DogTest.java
@@ -0,0 +1,13 @@
+
+public class DogTest {
+	public static void main(String[] args) {
+		Dog dog = new Dog(3, "fido", true);
+		System.out.println(dog);
+		dog.setAge(5);
+		System.out.println(dog);
+		dog.setName("odi");
+		System.out.println(dog);
+		dog.setIsHappy(false);
+		System.out.println(dog);
+	}
+}
