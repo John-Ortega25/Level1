1258a1fd85d59e5fc6ed8d3a429c97078b16b127
John-Ortega25 <league@Mac-mini-3.local>
2017-08-19 12:19:22 -0700

    8-19-17

--- src/SimonSaysRecipe.java
+++ src/SimonSaysRecipe.java
@@ -10,7 +10,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import java.io.IOException;
 
 public class SimonSaysRecipe extends KeyAdapter {
 
@@ -21,10 +20,8 @@ public class SimonSaysRecipe extends KeyAdapter {
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
-	private int simonSays = 0;
 	private int randomNum;
 	Date timeAtStart;
-	
 
 	private void makeAlbum() {
 		// 2. add 4 images which match keyboard keys like this: images.put(new
@@ -38,25 +35,24 @@ public class SimonSaysRecipe extends KeyAdapter {
 		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
 		// 4. call the method to show an image
 		showImage();
-		
-		
+
 	}
 
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		// 16. make a points variable to track the score. tell the user their
 		// score at the end.
-		int points=0;
+		int points = 0;
 		// 17. if the keyCode matches the imageIndex and "Simon says..."
 		// increase their score
-		if(keyCode==imageIndex&&  randomNum>4){
-			points=points+1;
+		if (keyCode == imageIndex && simonSays()) {
+			points = points + 1;
 			speak("You are correct");
 		}
 		// 18. if the keyCode doesn't match the imageIndex and "Simon didn't
 		// say..." increase their score
-		else if(keyCode!= imageIndex&& this.randomNum <4){
-			points=points+1;
+		else if (keyCode != imageIndex && this.randomNum < 4) {
+			points = points + 1;
 			speak("You are correct");
 		}
 		// 19. Use the speak method to tell the user if they were correct or not
@@ -64,13 +60,13 @@ public class SimonSaysRecipe extends KeyAdapter {
 			speak("You are incorrect");
 		}
 		// 13. increment tries by 1
-		this.tries=tries+1;
+		this.tries = tries + 1;
 		// 14. if tries is greater than 9 (or however many you want)
-		if(tries>9){
+		if (tries > 9) {
 			System.exit(0);
 		}
 		// 15. exit the program
-	
+
 		// 11. dispose of the frame
 		frame.dispose();
 		// 12. call the method to show an image
@@ -90,17 +86,20 @@ public class SimonSaysRecipe extends KeyAdapter {
 		frame.addKeyListener(this);
 		// 10. Use the speak method to either say "Simon says press this key" or
 		// "Press this key"
-		  
+
 		// Hint: use the simonSays int and a random number
-		Random generator= new Random();
-		randomNum=generator.nextInt(11);
-		if(randomNum>4 ){
-			speak("Simon says press this key");	
-		}else{
-			speak("Press this key");	
+		Random generator = new Random();
+		randomNum = generator.nextInt(11);
+		if (simonSays()) {
+			speak("Simon says press this key");
+		} else {
+			speak("Press this key");
 		}
-		
-		
+
+	}
+
+	private boolean simonSays() {
+		return randomNum > 4;
 	}
 
 	private Component getNextRandomImage() {
@@ -128,10 +127,8 @@ public class SimonSaysRecipe extends KeyAdapter {
 }
 
 /*
- * 20. add a timer ~~~ where the code starts running ~~~ timeAtStart = new
- * Date();
+ * 20. add a timer ~~~ where the code starts running ~~~ timeAtStart = new Date();
  *
  * ~~~ where the code ends ~~~ Date timeAtEnd = new Date();
- * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000);
- * System.exit(0);
+ * System.out.println((timeAtEnd.getTime()-timeAtStart.getTime())/1000); System.exit(0);
  */
