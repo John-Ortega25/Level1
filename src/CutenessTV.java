7e52cad47e99ebb44eee67f70ffacda813fa7864
John-Ortega25 <league@Mac-mini-3.local>
2017-08-26 12:02:02 -0700

    8-26-17

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -7,19 +7,38 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class CutenessTV implements ActionListener {
+	JButton duckButton= new JButton();
+	JButton fluffyUnicorn= new JButton();
+	JButton frogButton= new JButton();
+	
+	public static void main(String[] args) {
+		CutenessTV tv = new CutenessTV();
+		
+		tv.createUI();
+	}
 
-public static void main(String[] args) {
-	CutenessTV tv= new CutenessTV();
-	tv.createUI();
-}
-
-private void createUI() {
-	JFrame frame= new JFrame();
-	JPanel panel= new JPanel();
-	frame.setVisible(true);
-	frame.add(panel);
-	frame.pack();
-}
+	private void createUI() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setSize(500, 500);
+		
+		duckButton.addActionListener(this);
+		duckButton.setText("Duck");
+		panel.add(duckButton);
+		
+		fluffyUnicorn.addActionListener(this);
+		fluffyUnicorn.setText("Fluffy Unicorn");
+		panel.add(fluffyUnicorn);
+		
+		frogButton.addActionListener(this);
+		frogButton.setText("Frog");
+		panel.add(frogButton);
+		
+		
+		frame.pack();
+	}
 
 	void showDucks() {
 		playVideo("Drirjl5K9Yk");
@@ -35,7 +54,7 @@ private void createUI() {
 
 	void playVideo(String videoID) {
 		try {
-			URI uri = new URI("https://www.youtube.com/v/" + videoID + "?autoplay=1");
+			URI uri = new URI("https://www.youtube.com/watch?v=" + videoID + "?autoplay=1");
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,7 +64,14 @@ private void createUI() {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton) e.getSource();
-
+		if (buttonPressed== duckButton) {
+			showDucks();
+		}
+		else if (buttonPressed== frogButton) {
+			showFrog();
+		} else {
+			showFluffyUnicorns();
+		}
 	}
 
 }
